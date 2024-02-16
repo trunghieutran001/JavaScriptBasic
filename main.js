ff69f272d42f34b46bca8232b8780b699c066cc4
ヒエウ チャンチュン <N2207@nissin-mfg.co.jp>
2024-02-16 10:10:31 +0900

    Logical operators and if conditional statements

--- main.js
+++ main.js
@@ -1,63 +1,30 @@
+// Hiểu hơn về câu lệnh điều kiện
+// Và phép o sánh
+
 /*
-Kiểu dữ liệu trong JavaScript
-
-1. Kiểu dữ liệu nguyên thủy - Primitive Data
-    - Number
-    - String
-    - Boolean
-    - Underfined
-    - Null
-    - Symbol
-
-2. Dữ liệu phức tạp - Complex Data
-    - Function
-    - Object
+    0
+    ''
+    null
+    undefined
+    NaN
+    false
 */
-
-//Number type
 var a = 1;
 var b = 2;
-var c = 1.5;
 
-console.log(typeof a);
+//AND
+var result1 = 'A' && 'B' && 'C';
+var result2 = null && 'B' && 'C';
 
-//string type
-var fullName = 'Trung Hieu Tran';
-console.log(typeof fullName);
+//OR
+var result3 = 'A' || undefined || 'C';
 
-// Boolean type
-var isSuccess = false;
-console.log(typeof isSuccess);
+console.log(result1);
+console.log(result2);
 
-// undefined type
-var age;
-console.log(typeof age);
-
-// Null
-var isNull = null;
-
-//Symbol
-var id = Symbol('id'); //unique
-var id2 = Symbol('id'); //unique
-
-//Function
-var myFunction = function () {
-    alert('Hi. Xin choa cac ban');
-};
-console.log('Function type: ', typeof Object);
-
-//Object types
-var myObject = {
-    name: 'Tran Hieu',
-    age: 18,
-    address: 'Ha Noi',
-    myFunction: function () { }
-
-};
-console.log('Object: ', myObject);
-console.log('Object type: ', typeof myObject);
-
-//Array type
-var myArray = ['JavaScript', 'PHP', 'Ruby'];
-console.log(myArray);
-console.log('Array type: ', typeof myArray);
+if (result3) {
+    console.log('Dieu kien dung');
+}
+else {
+    console.log('Dieu kien sai');
+}
