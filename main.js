1f305700319ca03d03f3deeeb21d13a7f877d984
ヒエウ チャンチュン <N2207@nissin-mfg.co.jp>
2024-02-19 09:28:32 +0900

    what is function

--- main.js
+++ main.js
@@ -1,55 +1,24 @@
-// Làm việc với chuỗi
+/*
+1. Hàm?
+    - Một khối mã
+    - Làm 1 việc cụ thể
 
-var myString = 'Hoc JS tai F8!';
+2. Loại hàm
+    - Built-in
+    - Tự định nghĩa
 
-//Keyword: JavaScript strign methods
+3. Tính chất
+    - Không thực thi khi định nghĩa
+    - Sẽ thực thi khi được gọi
+    - Có thể nhận tham số
+    - Có thể trả về 1 giá trị
 
-// 1. Length
-console.log(myString.length);
+4. Tạo hàm đầu tiên
+*/
 
-// 2. Find index
-var myString = 'Hoc JS tai JS JS F8!';
-console.log(myString.indexOf('JS'));
-console.log(myString.lastIndexOf('JS'));
-console.log(myString.indexOf('JS', 6));//tim tai vi tri bat dau, 6 o dau la vi tri bat dau
-console.log(myString.indexOf('ABC')); // Khong tim thay se tra ve -1
-console.log(myString.search('JS'));// Ho tro tiem kiem voi RegExp
+// a-z A-Z 0-9 _ $
 
-// 3. Cut string
-var myString = 'Hoc JS tai F8!';
-console.log(myString.slice(4, 6));// Tham so 1: la vi tri bat dau cut, tham so 2: La cut den vi tri
-console.log(myString.slice(4));
-console.log(myString.slice(0));
-console.log(myString.slice(-3, -1));
-
-// 4. Replace
-var myString = 'Hoc JS tai F8!';
-console.log(myString.replace('JS', 'JavaScript'))
-
-var myString = 'Hoc JS tai JS JS F8!';
-console.log(myString.replace(/JS/g, 'JavaScript'))
-
-
-// 5. Convert to upper case
-var myString = 'Hoc JS tai F8!';
-console.log(myString.toLocaleUpperCase());
-
-// 6. Convert to lower case
-console.log(myString.toLowerCase());
-
-// 7. Trim
-var myString = '   Hoc JS tai F8!   ';
-console.log(myString)
-console.log(myString.length);
-console.log(myString.trim());
-console.log(myString.trim().length);
-
-// 8. Split
-var languages = 'JavaScript, PHP, Ruby';
-console.log(languages.split(', '))
-var languages = 'JavaScript';
-console.log(languages.split(''))
-
-// 9. Get a character by index
-var myString = 'Hoc JS tai F8!';
-console.log(myString[10]);
+function showDialog() {
+    alert('Hi xin chao cac ban')
+};
+console.log(showDialog())
