f99d379c108bdf1c13e213765ae0f21fb180bd43
ヒエウ チャンチュン <N2207@nissin-mfg.co.jp>
2024-02-23 10:03:34 +0900

    Object Prototype

--- main.js
+++ main.js
@@ -1,22 +1,25 @@
-// Object constructor
-
+/*
+    Object prototype - Basic
+    1. Prototype là gì?
+    2. Sử dụng khi nào?
+*/
 function User(firstName, lastName, avatar) {
     this.firstName = firstName;
     this.lastName = lastName;
     this.avatar = avatar;
-
     this.getName = function () {
-        return `${this.firstName} ${this.lastName}`
+        return `${this.firstName} ${this.lastName}`;
     }
+} s
+
+User.prototype.className = 'F8';
+User.prototype.getClassName = function () {
+    return this.className;
 }
 
-var author = new User('Hieu', 'Tran', 'Avatar');
-var user = new User('Vu', 'Nguyen', 'Avatar');
+var user = new User('Hieu', 'Tran', 'Avatar 1');
+var user1 = new User('Howard', 'Tran', 'Avatar 2');
 
-author.title = 'Hello member';
-user.comment = 'Xin chao admin';
-console.log(author);
-console.log(user);
 
-console.log(author.getName());
-console.log(user.getName());
+console.log(user.className);
+console.log(user1.getClassName());
