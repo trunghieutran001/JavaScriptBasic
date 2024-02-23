8f5f671eb9b870a5c15ab330f475c31b6b8689a8
ヒエウ チャンチュン <N2207@nissin-mfg.co.jp>
2024-02-23 14:12:38 +0900

    continue

--- main.js
+++ main.js
@@ -1,14 +1,11 @@
-// do while loop
+// Continue in Loop
+for (var i = 0; i < 10; i++) {
 
-var a = 0;
-var isSuccess = false;
-
-do {
-    a++;
-    console.log('Nạp thẻ lần ' + a);
-    // Thành công
-    if (false) {
-        isSuccess = true;
+    if (i % 2 == 0) {
+        continue;
+        // Nếu là số chẵn thì sẽ bỏ qua
+        // các the command below và sẽ quay trở lại vòng loop
     }
 
-} while (!isSuccess && a <= 3);
+    console.log(i);
+}
