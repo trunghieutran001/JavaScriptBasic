a3c138db43fd5c63f4c4cb335512a253b665da70
ヒエウ チャンチュン <N2207@nissin-mfg.co.jp>
2024-02-28 11:27:53 +0900

    my filter

--- main.js
+++ main.js
@@ -1,16 +1,46 @@
 // forEach, find, filter, some, every, reduce
 
-// forEach
-Array.prototype.forEach2 = function (callback) {
+// find2()
+Array.prototype.filter2 = function (callback) {
+    var output = [];
     for (var index in this) {
         if (this.hasOwnProperty(index)) {
-            callback(this[index], index, this);
+            var result = callback(this[index], index, this)
+        }
+        if (result) {
+            output.push(this[index]);
         }
     }
+    return output;
 }
-var courses = new Array(100);
-courses.push('JavaScript', 'Ruby');
-
-courses.forEach2(function (course, index, array) {
-    console.log(course, index, array);
+var courses = [
+    {
+        id: 1,
+        name: 'JavaScript',
+        coin: 200
+    },
+    {
+        id: 2,
+        name: 'HTML, CSS',
+        coin: 0
+    },
+    {
+        id: 3,
+        name: 'Ruby',
+        coin: 250
+    },
+    {
+        id: 4,
+        name: 'PHP',
+        coin: 400
+    },
+    {
+        id: 5,
+        name: 'ReactJS',
+        coin: 500
+    }
+];
+var filterCourses = courses.filter2(function (course, index, array) {
+    return course.coin > 250;
 });
+console.log(filterCourses);
